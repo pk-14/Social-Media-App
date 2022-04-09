9a17ce6cf76eea6cd9b938501e933969b31c7b64
pk-14 <100840901+pk-14@users.noreply.github.com>
2022-04-10 02:08:47 +0530

    Add files via upload

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGOURL : process.env.MONGOURL,
-    JWT_SECRET : process.env.JWT_SEC
+    JWT_SECRET : process.env.JWT_SEC,
+    NODEMAILER_PASS : process.env.NODEMAILER_PASS
 };
--- routes/user.js
+++ routes/user.js
@@ -66,4 +66,15 @@ router.put("/updatepic", requireLogin, (req, res) => {
     )
 })
 
+router.post('/searchusers',(req,res)=>{
+    let userPattern = new RegExp("^"+req.body.query)
+    User.find({name:{$regex:userPattern}})
+    .select(" _id email name")
+    .then(user=>{
+        res.json({user})
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
 module.exports = router;
